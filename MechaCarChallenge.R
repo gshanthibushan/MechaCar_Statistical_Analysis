ed328905baa500fd2c82e935b29dc02dbc777290
Geetha Shanthibushan <79486450+gshanthibushan@users.noreply.github.com>
2021-06-13 22:15:29 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,6 +12,9 @@ library(pastecs)
 install.packages("dplyr")
 library(dplyr)
 
+install.packages("gtsummary")
+library(gtsummary)
+
 #Import and read
 MechaCar <- read.csv(file='MechaCar_mpg.csv', check.names=T, stringsAsFactors = F)
 
@@ -21,3 +24,21 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #Perform summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
 
+
+# Deliverable 2: Create Visualizations for the Trip Analysis 
+#Import and read
+SuspensionCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#Write an RScript that creates a total_summary dataframe
+total_summary <- SuspensionCoil %>% summarize(Mean=mean(PSI),
+                                          Median=median(PSI),
+                                          Variance=var(PSI),
+                                          SD=sd(PSI), .groups = 'keep') 
+total_summary
+
+#Write an RScript that creates a lot_summary dataframe
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                              Median=median(PSI),
+                                              Variance=var(PSI),
+                                              SD=sd(PSI), .groups = 'keep') 
+lot_summary
