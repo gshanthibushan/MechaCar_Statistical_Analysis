dcc95f59494da17e781b28192d12cd15f6b67594
Geetha Shanthibushan <79486450+gshanthibushan@users.noreply.github.com>
2021-06-13 23:53:23 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -42,3 +42,19 @@ lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean
                                               Variance=var(PSI),
                                               SD=sd(PSI), .groups = 'keep') 
 lot_summary
+
+
+#Deliverable 3: T-Tests on Suspension Coils
+#RScript using the t.test() function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(SuspensionCoil$PSI,mu=1500)
+
+#RScript using the t.test() function and its subset() argument to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+lot1 <- subset(SuspensionCoil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(SuspensionCoil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(SuspensionCoil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
+
+
